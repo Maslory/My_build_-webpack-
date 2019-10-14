5372a2d8873454e7e310be43bb565d2d2cdc4e22
Maslory <stanlisword@gmail.com>
2019-10-14 09:58:44 +0500

    first commit

---
+++ src/app/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../style/style.sass';
+
+
+
+
+
+class App extends React.Component {
+    // constructor(props){
+     
+    // }
+  
+  
+  
+    render() {
+     
+      return (
+       <div>Привет-мавлет!</div>
+      );
+    }
+  }
+
+export default App;
---
+++ src/app/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../style/style.sass';
+import App from './app';
+
+
+// ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));   
